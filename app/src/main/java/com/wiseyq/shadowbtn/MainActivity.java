6f8d69a2e4f5502f9e22e1dac87fb2b0dbe9d697
luffyjet <luffyjet@foxmail.com>
2017-10-26 16:41:18 +0800

    init

---
+++ app/src/main/java/com/wiseyq/shadowbtn/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wiseyq.shadowbtn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "登录！", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
