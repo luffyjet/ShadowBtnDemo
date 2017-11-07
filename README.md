b3d5eb84089cba10fa44dbb777f26e39823df294
luffyjet <luffyjet@foxmail.com>
2017-11-07 18:53:33 +0800

    新增实现方式，可灵活的设置阴影颜色和大小

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 用CardView实现带阴影的按钮
-附带点击效果，5.0以上带ripple效果和点击效果，5.0以下只有点击效果。
+# 带阴影的按钮的实现
+1.用CardView实现
+2.用CardViewGingerbread里的 RoundRectDrawableWithShadow 实现，可灵活的设置阴影颜色和大小,可设置StateListDrawable。
+
--- app/src/main/java/com/wiseyq/shadowbtn/MainActivity.java
+++ app/src/main/java/com/wiseyq/shadowbtn/MainActivity.java
@@ -1,21 +1,66 @@
 package com.wiseyq.shadowbtn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private View drawView1, drawView2, selectorView;
+    int normalColor = Color.parseColor("#959595");
+    int enableColor = Color.parseColor("#ff9b1a");
+    int shadowSize = 6;//dp
+    int cornerSize = 5;//dp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+        initShadow();
+    }
+
+    private void initShadow() {
+        ShadowUtil.setShadowBg(drawView1,normalColor,normalColor,cornerSize,shadowSize,shadowSize);
+        ShadowUtil.setShadowBg(drawView2,enableColor,enableColor,cornerSize,shadowSize,shadowSize);
+        ShadowUtil.setShadowSelectorBg(selectorView,normalColor,enableColor,normalColor,enableColor,cornerSize,shadowSize,shadowSize);
+    }
+
+
+    private void initView() {
+        drawView1 = findViewById(R.id.relay_draw1);
+        drawView2 = findViewById(R.id.relay_draw2);
+        selectorView = findViewById(R.id.relay_draw3);
+
         findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "登录！", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "CardView实现方式！", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void initListener() {
+        drawView1.setOnClickListener(this);
+        drawView2.setOnClickListener(this);
+        selectorView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.relay_draw1:
+
+                break;
+            case R.id.relay_draw2:
+
+                break;
+            case R.id.relay_draw3:
+
+                break;
+        }
     }
 }
